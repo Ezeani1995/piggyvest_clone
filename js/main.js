fc183ae457781fc61af0ae67c282766da2924de9
Ezeani Anthony Ikechukwu <ezeani.ikechukwu1995@gmail.com>
2022-06-08 22:58:05 +0100

    First commit of the piggyvest website clone project

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const header = document.querySelector("#header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) header.classList.add("scrollY");
+  else header.classList.remove("scrollY");
+});
