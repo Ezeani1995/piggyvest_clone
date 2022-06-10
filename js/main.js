386bb99261119ebfce59b1acab07273b3323355b
Ezeani Anthony Ikechukwu <ezeani.ikechukwu1995@gmail.com>
2022-06-10 23:33:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
 const header = document.querySelector("#header");
-const testimonialMessagesContainer = document.querySelector(
-  ".testimonials-messages"
-);
-const testimonialMessages = document.querySelectorAll(".testimonials-message");
-const maxHeight = 833;
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) header.classList.add("scrollY");
   else header.classList.remove("scrollY");
 });
 
-console.log(testimonialMessagesContainer.clientHeight);
-console.log(testimonialMessages);
-
-testimonialMessages.forEach((testimonialMessage) => {
-  console.log(testimonialMessage.clientHeight);
-});
-
-function getNextDiv() {
-  for (let i = 0; i < testimonialMessages.length; i++) {}
-}
